87c8634d65d404b224728aea3ebb59d61877144e
Tarek Saier <tareksaier@gmail.com>
2025-11-23 16:15:35 +0900

    set CC0 license

--- data/make_js.sh
+++ data/make_js.sh
@@ -2,6 +2,8 @@
 
 out_fn="../docs/js/data.js"
 
+echo "// Cited texts from literature sources in the JSON files may be subject to" > $out_fn
+echo "// license terms. Please check the provided source URLs in case of re-use." > $out_fn
 echo "data = [" > $out_fn
 for fn in ./*.json; do
     cat "$fn" >> $out_fn
